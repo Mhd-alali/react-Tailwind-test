4fabde42bd5224757543cf58b6b334f4176a4f76
Muhammed_alali <72269280+Mhd-alali@users.noreply.github.com>
2023-07-17 04:53:37 -0700

    uncommented section.jsx

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -13,12 +13,12 @@ export function Section({section,recipes}) {
       </div>
       <div className="container mt-8 grid md:grid-cols-3 gap-5">
         {recipes && recipes.map((recipe)=>  <Card ID={recipe.id} key={recipe.id} dishTypes={recipe.dishTypes} recipe={recipe.title} time={recipe.readyInMinutes} imgSrc={recipe.image} /> )}
-         {/* <Card ID={23} dishTypes={["Type1","Type2","Type3"]} recipe={"recipe.title"} time={45} imgSrc={"./img/1.webp"} />
+         <Card ID={23} dishTypes={["Type1","Type2","Type3"]} recipe={"recipe.title"} time={45} imgSrc={"./img/1.webp"} />
          <Card ID={23} dishTypes={["Type1","Type2","Type3"]} recipe={"recipe.title"} time={45} imgSrc={"./img/2.jpg"} />
          <Card ID={23} dishTypes={["Type1","Type2","Type3"]} recipe={"recipe.title"} time={45} imgSrc={"./img/3.jpg"} />
          <Card ID={23} dishTypes={["Type1","Type2","Type3"]} recipe={"recipe.title"} time={45} imgSrc={"./img/4.webp"} />
          <Card ID={23} dishTypes={["Type1","Type2","Type3"]} recipe={"recipe.title"} time={45} imgSrc={"./img/5.jpg"} />
-         <Card ID={23} dishTypes={["Type1","Type2","Type3"]} recipe={"recipe.title"} time={45} imgSrc={"./img/6.jpg"} /> */}
+         <Card ID={23} dishTypes={["Type1","Type2","Type3"]} recipe={"recipe.title"} time={45} imgSrc={"./img/6.jpg"} />
         <div className='flex justify-center my-5 md:col-span-3'>
           <Button className="px-10 py-3" type="primary">Load More</Button>
         </div>
